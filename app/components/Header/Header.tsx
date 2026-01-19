99cd0522cab69a32f952a520ceaed0c02a2519d2
satya verma <satyaverma0075@gmail.com>
2026-01-20 02:11:25 +0530

    updates

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -14,12 +14,12 @@ export function Header() {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 border-b border-gray-200 dark:border-gray-800 bg-[#f9fafb]/80 dark:bg-gray-950/80 backdrop-blur-sm">
-      <div className="max-w-7xl mx-auto  h-18 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto  h-16 flex items-center justify-between">
         <Logo />
 
         <DesktopNav />
 
-        <div className="flex items-center gap-4 px-4">
+        <div className="flex items-center gap-4">
           <ThemeToggle />
           <LanguageSwitcher />
 
