9b307f72e45d4a1702eb162ae5394dd53f4f6124
satya verma <satyaverma0075@gmail.com>
2026-01-20 12:01:45 +0530

    updates

--- app/components/Header/Logo.tsx
+++ app/components/Header/Logo.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center mx-2">
+    <Link href="/" className="flex items-center ">
       <Image
         src="/Juice-2024-Logo-2000x800.png"
         alt="Juice"
-        width={80}
+        width={90}
         height={29}
         priority
       />
